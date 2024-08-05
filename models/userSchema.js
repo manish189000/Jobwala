20e55a41a8fb7b0d6dcc62218935ace47f615404
Manish Jha <jham8423@gmail.com>
2024-08-05 14:57:22 +0530

    all changes are doen

--- models/userSchema.js
+++ models/userSchema.js
@@ -58,9 +58,72 @@
 
 // export const User = mongoose.model("User", userSchema);
 
+// import mongoose from "mongoose";
+// import validator from "validator";
+// import bcrypt from "bcryptjs";
+// import jwt from "jsonwebtoken";
+
+// const userSchema = new mongoose.Schema({
+//   name: {
+//     type: String,
+//     required: [true, "Please enter your Name!"],
+//     minLength: [3, "Name must contain at least 3 Characters!"],
+//     maxLength: [30, "Name cannot exceed 30 Characters!"],
+//   },
+//   email: {
+//     type: String,
+//     required: [true, "Please enter your Email!"],
+//     validate: [validator.isEmail, "Please provide a valid Email!"],
+//   },
+//   phone: {
+//     type: Number,
+//     required: [true, "Please enter your Phone Number!"],
+//   },
+//   password: {
+//     type: String,
+//     required: [true, "Please provide a Password!"],
+//     minLength: [8, "Password must contain at least 8 characters!"],
+//     maxLength: [32, "Password cannot exceed 32 characters!"],
+//     select: false,
+//   },
+//   role: {
+//     type: String,
+//     required: [true, "Please select a role"],
+//     enum: ["Job Seeker", "Employer"],
+//   },
+//   createdAt: {
+//     type: Date,
+//     default: Date.now,
+//   },
+// });
+
+// //ENCRYPTING THE PASSWORD WHEN THE USER REGISTERS OR MODIFIES HIS PASSWORD
+// userSchema.pre("save", async function (next) {
+//   if (!this.isModified("password")) {
+//     next();
+//   }
+//   this.password = await bcrypt.hash(this.password, 10);
+// });
+
+// //COMPARING THE USER PASSWORD ENTERED BY USER WITH THE USER SAVED PASSWORD
+// userSchema.methods.comparePassword = async function (enteredPassword) {
+//   return await bcrypt.compare(enteredPassword, this.password);
+// };
+
+// //GENERATING A JWT TOKEN WHEN A USER REGISTERS OR LOGINS, IT DEPENDS ON OUR CODE THAT WHEN DO WE NEED TO GENERATE THE JWT TOKEN WHEN THE USER LOGIN OR REGISTER OR FOR BOTH.
+// userSchema.methods.getJWTToken = function () {
+//   return jwt.sign({ id: this._id }, process.env.JWT_SECRET_KEY, {
+//     expiresIn: process.env.JWT_EXPIRE,
+//   });
+// };
+
+// export const User = mongoose.model("User", userSchema);
+
+//testing
+
 import mongoose from "mongoose";
 import validator from "validator";
-import bcrypt from "bcrypt";
+import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 
 const userSchema = new mongoose.Schema({
@@ -84,7 +147,7 @@ const userSchema = new mongoose.Schema({
     required: [true, "Please provide a Password!"],
     minLength: [8, "Password must contain at least 8 characters!"],
     maxLength: [32, "Password cannot exceed 32 characters!"],
-    select: false,
+    select: false, // Don't return the password by default
   },
   role: {
     type: String,
@@ -97,24 +160,39 @@ const userSchema = new mongoose.Schema({
   },
 });
 
-//ENCRYPTING THE PASSWORD WHEN THE USER REGISTERS OR MODIFIES HIS PASSWORD
+// Encrypt password before saving user
 userSchema.pre("save", async function (next) {
+  // Only hash the password if it has been modified (or is new)
   if (!this.isModified("password")) {
-    next();
+    return next();
   }
-  this.password = await bcrypt.hash(this.password, 10);
+
+  try {
+    // Hash the password with a cost factor of 10
+    this.password = await bcrypt.hash(this.password, 10);
+    next();
+  } catch (error) {
+    next(error);
+  }
 });
 
-//COMPARING THE USER PASSWORD ENTERED BY USER WITH THE USER SAVED PASSWORD
+// Compare entered password with hashed password
 userSchema.methods.comparePassword = async function (enteredPassword) {
-  return await bcrypt.compare(enteredPassword, this.password);
+  try {
+    // Compare the entered password with the hashed password
+    return await bcrypt.compare(enteredPassword, this.password);
+  } catch (error) {
+    throw new Error("Error comparing passwords");
+  }
 };
 
-//GENERATING A JWT TOKEN WHEN A USER REGISTERS OR LOGINS, IT DEPENDS ON OUR CODE THAT WHEN DO WE NEED TO GENERATE THE JWT TOKEN WHEN THE USER LOGIN OR REGISTER OR FOR BOTH.
+// Generate JWT token for user authentication
 userSchema.methods.getJWTToken = function () {
+  // Sign the token with user ID and secret key
   return jwt.sign({ id: this._id }, process.env.JWT_SECRET_KEY, {
-    expiresIn: process.env.JWT_EXPIRE,
+    expiresIn: process.env.JWT_EXPIRE, // Token expiration time
   });
 };
 
+// Export the User model
 export const User = mongoose.model("User", userSchema);
